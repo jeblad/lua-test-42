8a5c897b887171905acf5bfb78af87a5a5cef972
John Erling Blad <jeblad@gmail.com>
2020-06-07 19:41:34 +0200

    Modified main.lua

--- includes/Main.lua
+++
@@ -1,9 +0,0 @@
---- Define a main car, whatever that is.
---- @class Main
-local cls = class()
-
---- @param foo number
-function cls:test(foo)
-end
-
-return cls
---
+++ includes/main.lua
@@ -0,0 +1,22 @@
+--- Define a main car, whatever that is.
+---@module main
+local main = {}
+
+local h = {}
+
+--- This is desc of test.
+---@param foo number and more text
+function main:test(foo)
+end
+
+--- This is desc of ping.
+---@param foo number and more text
+function main:ping(foo)
+end
+
+--- This is desc of pong.
+---@param foo number and more text
+function main:pong(foo)
+end
+
+return main
